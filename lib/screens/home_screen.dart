1c5d06a23cc9ba82018c593e358500a2ef680ba3
erfankeshavarz0085 <erfankeshavarz0085@gmail.com>
2026-07-05 18:06:41 +0330

    Add initial providers and splash screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,11 +5,11 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: Text('Football Results'),
+        title: const Text('Football Results'),
       ),
-      body: Center(
+      body: const Center(
         child: Text('Home Screen آماده است'),
       ),
     );
