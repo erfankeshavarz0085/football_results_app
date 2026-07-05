d1492f6a83c1cd06b91ab04257d6bcd5d6bd679d
erfankeshavarz0085 <erfankeshavarz0085@gmail.com>
2026-07-05 21:23:30 +0330

    Add fixture provider API loading

--- lib/providers/fixture_provider.dart
+++ lib/providers/fixture_provider.dart
@@ -1,6 +1,44 @@
 import 'package:flutter/material.dart';
 
+import '../models/fixture_model.dart';
+import '../services/api_service.dart';
+
 class FixtureProvider extends ChangeNotifier {
+  final ApiService _apiService = ApiService();
+
   bool isLoading = false;
   String? errorMessage;
+
+  List<FixtureModel> todayFixtures = [];
+  List<FixtureModel> liveFixtures = [];
+
+  Future<void> loadTodayFixtures() async {
+    isLoading = true;
+    errorMessage = null;
+    notifyListeners();
+
+    try {
+      todayFixtures = await _apiService.getTodayFixtures();
+    } catch (e) {
+      errorMessage = e.toString();
+    }
+
+    isLoading = false;
+    notifyListeners();
+  }
+
+  Future<void> loadLiveFixtures() async {
+    isLoading = true;
+    errorMessage = null;
+    notifyListeners();
+
+    try {
+      liveFixtures = await _apiService.getLiveFixtures();
+    } catch (e) {
+      errorMessage = e.toString();
+    }
+
+    isLoading = false;
+    notifyListeners();
+  }
 }
