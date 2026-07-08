39b629088ae0c69a5c15d6e9c7d9b31302473e3f
erfankeshavarz0085 <erfankeshavarz0085@gmail.com>
2026-07-08 13:11:18 +0330

    Secure API key with dotenv

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,16 +1,36 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+
 class AppConstants {
-  // API
-  static const String apiKey = "7da1d62d3a3b98ff9c82af09ebda9e46";
-  static const String baseUrl = "https://v3.football.api-sports.io";
+
+
+  static String get apiKey {
+
+    return dotenv.env['API_KEY'] ?? '';
+
+  }
+
+
+
+  static const String baseUrl =
+      "https://v3.football.api-sports.io";
+
+
 
   // لیگ‌ها
+
   static const int premierLeague = 39;
+
   static const int laLiga = 140;
+
   static const int serieA = 135;
+
   static const int bundesliga = 78;
+
   static const int ligue1 = 61;
+
   static const int championsLeague = 2;
 
-  // فصل
-  static const int season = 2024;
+
+
 }
