93ba483e4a49042bd9ac024bc6cb88979bd6ed37
erfankeshavarz0085 <erfankeshavarz0085@gmail.com>
2026-07-08 14:21:22 +0330

    Complete match details and polish league logos

--- lib/screens/leagues_screen.dart
+++ lib/screens/leagues_screen.dart
@@ -172,7 +172,7 @@ class _LeaguesScreenState extends State<LeaguesScreen> {
           width: 48,
           height: 48,
           decoration: BoxDecoration(
-            color: const Color(0xff0d1117),
+            color: Colors.white,
             borderRadius: BorderRadius.circular(16),
             border: Border.all(color: Colors.white10),
           ),
@@ -183,7 +183,7 @@ class _LeaguesScreenState extends State<LeaguesScreen> {
               fit: BoxFit.contain,
               errorWidget: (_, __, ___) => Icon(
                 league.fallbackIcon,
-                color: Colors.greenAccent,
+                color: Colors.black,
               ),
             ),
           ),
